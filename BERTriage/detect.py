d1544f9269f0d800e362c977868a85e5548612b2
IAmGreyBunny <iamgreybunny@gmail.com>
2022-03-20 00:01:17 +0800

    Added flask webview

--- BERTriage/detect.py
+++ BERTriage/detect.py
@@ -8,6 +8,7 @@ physical_devices = tf.config.list_physical_devices('GPU')
 for device in physical_devices:
     tf.config.experimental.set_memory_growth(device, True)
 
+
 def load_model(model_path):
     model = create_model()
     model.load_weights(model_path)
--- Webapp/main.py
+++ Webapp/main.py
@@ -1,17 +1,20 @@
-from flask import Flask
+from flask import Flask,render_template,url_for
 from flask_restful import Api, Resource,reqparse
 
 import BERTriage.detect
 
-#Loads model
+# Loads NLP model
 nlp_model = BERTriage.detect.load_model(r"D:\ICT Competition\Model\model.hdf5")
 
+# Initializing flask
 app = Flask(__name__)
 api = Api(app)
 
+# Configuring bertriage api call
 bertriage_api_args = reqparse.RequestParser()
 bertriage_api_args.add_argument("data",type=str,help="Sentence Data is missing",required=True)
 
+# Bertriage api resource object
 class bertriage_api(Resource):
     def post(self):
         args = bertriage_api_args.parse_args()
@@ -20,8 +23,15 @@ class bertriage_api(Resource):
         print(BERTriage.detect.make_prediction(nlp_model,args["data"]))
         return
 
-
+# Add bertriage api
 api.add_resource(bertriage_api,"/bertriage_api")
+
+
+@app.route("/")
+def authentication_page():
+    return render_template('authentication_page.html')
+    #return "Hello World"
+
 
 if __name__ == "__main__":
     app.run(debug=True)
