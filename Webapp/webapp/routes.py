2cb72717921f8313e7c11258cc5261fd2fd2056d
IAmGreyBunny <iamgreybunny@gmail.com>
2022-03-21 05:07:44 +0800

    Partially working webapp

--- Webapp/webapp/routes.py
+++ Webapp/webapp/routes.py
@@ -1,7 +1,44 @@
-from flask import render_template,url_for
+from flask import render_template,url_for,request, redirect
 from webapp import app
+from webapp.models import Patient, Bed
+from webapp import db
 
 
-@app.route("/")
-def authentication_page():
-    return render_template('authentication_page.html')
+
+#@app.route("/")
+#def authentication_page():
+#    return render_template('authentication_page.html')
+
+@app.route("/", methods=['GET'])
+def home():
+    patients = Patient.query.all()
+    return render_template('home.html', patients=patients)
+
+
+@app.route('/<int:id>/delete', methods=['GET', 'POST'])
+def delete_patient(id):
+    patients = Patient.query.filter_by(patient_id=id).first()
+
+    try:
+        db.session.delete(patients)
+        db.session.commit()
+        return redirect('/')
+    except:
+        return "There was an error"
+
+
+@app.route('/<int:id>/edit', methods=['GET', 'POST'])
+def edit_patient(id):
+    patient = Patient.query.filter_by(patient_id=id).first()
+
+    if request.method == 'POST':
+        if patient:
+            patient.triage_category = request.form['triage_category']
+            patient.status = request.form['status']
+            patient.bed.bed_id = request.form['bed_id']
+            patient.bed.ward = request.form['ward']
+
+        db.session.commit()
+        return redirect('/')
+
+    return render_template('edit.html', patient=patient)
