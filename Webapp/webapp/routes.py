6a2d968b8050be7bec81e7127d040f541847c495
IAmGreyBunny <iamgreybunny@gmail.com>
2022-03-21 15:23:39 +0800

    Fixed edit bed and wards form

--- Webapp/webapp/routes.py
+++ Webapp/webapp/routes.py
@@ -1,12 +1,11 @@
-from flask import render_template,url_for,request, redirect
+from flask import render_template, url_for, request, redirect
 from webapp import app
 from webapp.models import Patient, Bed
 from webapp import db
 
 
-
-#@app.route("/")
-#def authentication_page():
+# @app.route("/")
+# def authentication_page():
 #    return render_template('authentication_page.html')
 
 @app.route("/", methods=['GET'])
@@ -35,8 +34,15 @@ def edit_patient(id):
         if patient:
             patient.triage_category = request.form['triage_category']
             patient.status = request.form['status']
-            patient.bed.bed_id = request.form['bed_id']
-            patient.bed.ward = request.form['ward']
+            if patient.patient_bed.ward is not request.form['ward']:
+                patient.patient_bed.ward = request.form['ward']
+                db.session.commit()
+            if int(patient.patient_bed.bed_id) != int(request.form['bed_id']):
+                print("Changing bed from: "+ str(patient.patient_bed.bed_id)+" to "+ str(request.form['bed_id']))
+                patient.patient_bed = None
+                new_bed = Bed(bed_id=request.form['bed_id'], ward=request.form['ward'])
+                patient.patient_bed = new_bed
+                db.session.commit()
 
         db.session.commit()
         return redirect('/')
